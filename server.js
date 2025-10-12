37e2148da48379dbf20b973879f4a4c8683d3676
gowthamkish <gowthamkish@outlook.com>
2025-10-12 11:07:34 +0530

    Added orders

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/admin", protect, isAdmin, adminRoutes);
-app.use("/api/order", protect, require("./routes/order"));
+app.use("/api/orders", protect, require("./routes/order"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
