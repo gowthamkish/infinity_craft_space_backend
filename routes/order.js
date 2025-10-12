35a283a9a39344c331d72628d53e0ef9cac21fc6
gowthamkish <gowthamkish@outlook.com>
2025-10-12 11:09:47 +0530

    Added orders

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Order = require("../models/Order");
 const router = express.Router();
 
-router.post("/place", async (req, res) => {
+router.post("/", async (req, res) => {
   const { userId, items } = req.body;
   const order = new Order({ userId, items });
   await order.save();
