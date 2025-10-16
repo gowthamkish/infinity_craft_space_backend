a297d6b725bd0239af1e0df676bb1c82fdb970e4
gowthamkish <gowthamkish@outlook.com>
2025-10-16 23:44:48 +0530

    Revert "Added google OAuth"
    This reverts commit abc233fb06864ce2a42425f2468047ee4712e9c9.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_APP_URL,
+  origin: process.env.FRONTEND_APP_URL || 'http://localhost:3000',
   credentials: true
 }));
 
